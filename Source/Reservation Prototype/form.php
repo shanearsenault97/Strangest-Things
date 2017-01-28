12fa50baac045daafd62b66c26d29310782bc0c3
Shane Arsenault <sarsenault112452@hollandcollege.com>
2017-01-28 15:33:48 -0400

    Added some database integration to the website prototype.

--- Source/Reservation Prototype/form.php
+++ Source/Reservation Prototype/form.php
@@ -12,13 +12,71 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Form</title>
 </head>
 <body>
-<form action="submit.php" method="post">
+<form action="submit.php" method="get">
     <label for="tableNum">Table Number:</label>
     <select name="tableNum" id="tableNum" required>
-        <option value="1">1</option>
+        <?php
+        //opens up database
+        @ $db = new mysqli('localhost', 'root', '', 'pork_shop');
+
+        //if the database does not connect, display custom-made error for security
+        if (mysqli_connect_errno()) {
+            echo 'Error: Could not connect to database.  Please try again later.</body></html>';
+            exit;
+        }
+
+
+        //query derived from form that gets search results
+        $query = "SELECT *
+                            FROM tables
+                            WHERE tableNum LIKE 'B%'
+                            OR tableNum LIKE  'T%'
+                            ";
+
+
+        //result of query
+        $result = $db->query($query);
+
+        //printing out results, along to check whether a row exists from the query
+        if (@$result->num_rows > 0) {
+
+
+            $boothCount = 1;
+            $tableCount = 1;
+
+            while ($row = $result->fetch_assoc()) {
+                var_dump($row['tableNum']);
+                $tableName = $row['tableNum'];
+
+                echo "<option value=$tableName>";
+                if (strpos($row['tableNum'], 'BTH') !== false) {
+                    echo "Booth " . $boothCount;
+                    $boothCount++;
+                } elseif (strpos($row['tableNum'], 'TBL') !== false) {
+                    echo "Table " . $tableCount;
+                    $tableCount++;
+                } else {
+                    echo "If this happened call the store to reserve your table.";
+                }
+
+                echo "</option>";
+            }
+
+        } else {
+
+            echo "<option value='placeholder'>Placeholder</option>";
+
+        }
+
+        //free up space in memory for the next query
+        @$result->free();
+
+        //closes the database for security
+        $db->close();
+        ?>
     </select><br/>
 
     <label for="date">Date:</label>
--- Source/Reservation Prototype/submit.php
+++ Source/Reservation Prototype/submit.php
@@ -7,26 +7,73 @@
  * Purpose: Submit Page for form, prototype reservation
  */
 
-$tableNum = $_POST['tableNum'];
-$date = $_POST['date'];
-$time = $_POST['time'];
-$name = $_POST['name'];
-$contactNumber = $_POST['contactNumber'];
+$tableNum = $_GET['tableNum'];
+$date = $_GET['date'];
+$time = $_GET['time'];
+$name = $_GET['name'];
+$contactNumber = $_GET['contactNumber'];
+
+
+
+echo "<div>Your submission is as follows:<br/>
+    Name: $name <br/>
+    Table Number: $tableNum<br/>
+    Date/Time: $date @ $time<br/>
+    Contact Number: $contactNumber<br/>
+</div>";
+
+//opens up database
+@ $db = new mysqli('localhost', 'root', '', 'pork_shop');
+
+//if the database does not connect, display custom-made error for security
+if (mysqli_connect_errno()) {
+    echo 'Error: Could not connect to database.  Please try again later.</body></html>';
+    exit;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Process</title>
 </head>
 <body>
-<div>Your reservation is as follows:<br/>
-    Name: <?php echo $name ?><br/>
-    Table Number: <?php echo $tableNum ?><br/>
-    Date/Time: <?php echo $date . " @ " . $time ?><br/>
-    Contact Number: <?php echo $contactNumber ?><br/>
-</div>
+<?php
+//query derived from form that gets search results
+$query = "SELECT * FROM reservation WHERE tableNum = '$tableNum' AND reservationDate = '$date' AND reservationTime = '$time'";
+
+//result of query
+$result = $db->query($query);
+
+//printing out results, along to check whether a row exists from the query
+if (@$result->num_rows > 0) {
+    echo "Reservation is already set for this table at the specified date and time.</body></html>";
+    die();
+}else {
+
+//free up space in memory for the next query
+    @$result->free();
+
+//query derived from form that gets search results
+    $query = "INSERT INTO reservation (tableNum, reservationDate, reservationTime, reservationName, reservationContact)
+VALUES ('$tableNum', '$date', '$time', '$name', '$contactNumber')";
+
+
+//result of query
+    $result = $db->query($query);
+
+//free up space in memory for the next query
+    @$result->free();
+
+//closes the database for security
+    $db->close();
+
+
+}
+?>
+
+
 <a href="form.php">Go Back to Form</a>
 </body>
 </html>
