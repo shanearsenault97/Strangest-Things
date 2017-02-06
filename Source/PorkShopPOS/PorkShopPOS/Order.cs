5e7a1e29894c62d0f8caaf654b988687a8c06b00
Noah Gallant <ngallant112507@hollandcollege.com>
2017-02-06 14:24:03 -0400

    Added Food+FoodDAO, Bar+BarDAO, and Order+OrderDAO

---
+++ Source/PorkShopPOS/PorkShopPOS/Order.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorkShopPOS {
+    class Order {
+        private string orderNum;
+
+        public string OrderNum {
+            get { return orderNum; }
+            set { orderNum = value; }
+        }
+        private string empNum;
+
+        public string EmpNum {
+            get { return empNum; }
+            set { empNum = value; }
+        }
+        private string tableNum;
+
+        public string TableNum {
+            get { return tableNum; }
+            set { tableNum = value; }
+        }
+        private string orderDate;
+
+        public string OrderDate {
+            get { return orderDate; }
+            set { orderDate = value; }
+        }
+        private string orderTime;
+
+        public string OrderTime {
+            get { return orderTime; }
+            set { orderTime = value; }
+        }
+        private string numGuests;
+
+        public string NumGuests {
+            get { return numGuests; }
+            set { numGuests = value; }
+        }
+        private string orderTotal;
+
+        public string OrderTotal {
+            get { return orderTotal; }
+            set { orderTotal = value; }
+        }
+        private string orderGratuity;
+
+        public string OrderGratuity {
+            get { return orderGratuity; }
+            set { orderGratuity = value; }
+        }
+
+        OrderDAO orderData;
+
+        public Order() {
+            orderData = new OrderDAO();
+        }
+    }
+}
